6effe4af857eee9671f173737c80e19fa0a184ce
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-06-02 18:10:10 +0530

    Change the customer text field

--- src/components/forms/theme-elements/CustomTextField.js
+++ src/components/forms/theme-elements/CustomTextField.js
@@ -3,25 +3,13 @@ import { styled } from '@mui/material/styles';
 import { TextField } from '@mui/material';
 
 const CustomTextField = styled((props) => <TextField {...props} />)(({ theme }) => ({
-  '& .MuiOutlinedInput-input::-webkit-input-placeholder': {
-    color: theme.palette.text.secondary,
-    opacity: '0',
-  },
-  '& .MuiOutlinedInput-input.Mui-disabled::-webkit-input-placeholder': {
-    color: theme.palette.text.secondary,
-    opacity: '1',
-  },
   '& .MuiOutlinedInput-root': {
-    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
-    transition: 'box-shadow 0.3s ease, padding 0.3s ease', // Add transitions for box shadow and padding
+    boxShadow: '0px 5px 5px rgba(0, 0, 0, 0.4)',
+    transition: 'box-shadow 1s ease, padding 1s ease',
     width: '100%',
-    padding: '2px', // Initial padding
-    height: 'auto', // Initial height
-  },
-  '&:hover .MuiOutlinedInput-root': {
-    boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.7)',
-    padding: '4px', // Expanded padding on hover
-    height: 'auto', // Expanded height on hover
+    padding: '1px',
+    height: 'auto',
+    elevation: 3,
   },
 }));
 
