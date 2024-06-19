c295e04e563f1ce2634be86fa9f2b4d693c85ab3
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-06-19 15:23:12 +0530

    Add links to header

--- src/views/Home/Header.js
+++ src/views/Home/Header.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 
 const Header = () => {
+  const handleLogin = () => {
+    window.location.href = '/auth/teacherLogin';
+  };
+
+  const handleRegister = () => {
+    window.location.href = '/auth/register';
+  };
+
   return (
     <AppBar position="static" color="primary">
       <Toolbar>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
           PaperGen
         </Typography>
-        <Button color="inherit">Login</Button>
-        <Button color="inherit">Register</Button>
+        <Button color="inherit" onClick={handleLogin}>
+          Login
+        </Button>
+        <Button color="inherit" onClick={handleRegister}>
+          Register
+        </Button>
       </Toolbar>
     </AppBar>
   );
