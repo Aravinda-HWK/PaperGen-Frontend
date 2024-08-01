39424d725137a47242dbd4b4b43dc529d4c9e33c
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-08-01 17:26:06 +0530

    Change base url

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 // config.js
-const baseURL = 'https://paper-gen-backend.vercel.app';
+const baseURL = 'https://paper-gen-backend.vercel.app/';
 // const baseURL = 'http://localhost:4999';
 export default baseURL;
