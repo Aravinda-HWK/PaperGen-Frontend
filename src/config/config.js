78285f733e11b270b8f6d822ed90fa76ada8531e
aravinda.20 <aravinda.20@cse.mrt.ac.lk>
2024-08-02 11:21:58 +0530

    update url

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
 // config.js
-const baseURL = 'papergen-backend-production.up.railway.app';
+const baseURL = 'https://papergen-backend-production.up.railway.app';
 // const baseURL = 'http://localhost:4999';
 export default baseURL;
